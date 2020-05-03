05cc9babcccdcd0d02405cdfb6eccc39470ec32c
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-03 16:20:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
  
  def self.genre_count   
    genre_count = hash.new(0)
-   @@genres.each {|genre| genre_count }
+   @@genres.each {|genre| genre_count[genre] +=1}
    genre_count
  end
  
